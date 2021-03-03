b0b69bb1b78af8515a97faa8061d52d646ad467a
rsmith <legendary.belief@gmail.com>
2021-03-02 19:55:14 -0800

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
+def my_collect(array)
+    i = 0
+   emt_array = []
+    while i < array.length
+      emt_array << (yield(array[i]))
+      i += 1
+    end
+    emt_array
+  end
+
+# my_collect(array) do |student|
+#     student.split(" ").first
 
